e64281e991d84965c5e59efc73da1ff2a63e95e6
Yashshree Rasal <yarasal@yarasal-thinkpadt14gen5.punetw6.csb>
2026-05-31 09:23:35 +0530

    modified-cgroup

--- health-check.sh
+++ health-check.sh
@@ -46,8 +46,7 @@ echo
 echo "===== CGroup Configuration ====="
 omc get nodes.config -oyaml | grep -i cgroup
 echo
-
-run_cmd "CGroup Mode Check" \ "echo 'To check current cgroup mode:' && \ echo && \ echo 'Reference:' && \ echo 'https://docs.redhat.com/en/documentation/openshift_container_platform/4.18/html/nodes/working-with-clusters#nodes-clusters-cgroups-2_nodes-cluster-cgroups-2' && \ echo 'https://access.redhat.com/solutions/7071862' && \ echo && \ echo 'Checking cgroup mode on all nodes...' && \ echo && \ for NODE in \$(oc get nodes -o name); do \ echo '------' \$NODE '------'; \ oc debug \$NODE -q -- chroot /host bash -c 'stat -c %T -f /sys/fs/cgroup'; \ echo; \ done && \ echo 'If output is cgroup2fs --> cgroup v2' && \ echo 'If output is tmpfs --> cgroup v1'"
+"CGroup Mode Check" \ "echo 'To check current cgroup mode:' && \ echo && \ echo 'Reference:' && \ echo 'https://docs.redhat.com/en/documentation/openshift_container_platform/4.18/html/nodes/working-with-clusters#nodes-clusters-cgroups-2_nodes-cluster-cgroups-2' && \ echo 'https://access.redhat.com/solutions/7071862' && \ echo && \ echo 'Checking cgroup mode on all nodes...' && \ echo && \ for NODE in \$(oc get nodes -o name); do \ echo '------' \$NODE '------'; \ oc debug \$NODE -q -- chroot /host bash -c 'stat -c %T -f /sys/fs/cgroup'; \ echo; \ done && \ echo 'If output is cgroup2fs --> cgroup v2' && \ echo 'If output is tmpfs --> cgroup v1'"
 
 read -p "Press Enter to continue..."
 
